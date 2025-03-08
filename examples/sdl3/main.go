8274af0ae2537c3931ef9d59e7aa0071f0a8efe8
Zyko <13394516+Zyko0@users.noreply.github.com>
2025-03-08 12:01:24 +0100

    SDL3 example

---
+++ examples/sdl3/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"runtime/debug"
+	"unsafe"
+
+	"github.com/totallygamerjet/clay"
+	sl "github.com/totallygamerjet/clay/examples/shared-layouts"
+	sdl2 "github.com/totallygamerjet/clay/renderers/sdl3"
+
+	sdl "github.com/Zyko0/go-sdl3"
+	"github.com/Zyko0/go-sdl3/ttf"
+)
+
+func handleClayError(errorText clay.ErrorData) {
+	slog.Error(errorText.ErrorText.String(), "stacktrace", debug.Stack())
+}
+
+// TODO: CreateArenaWithCapacityAndMemory should take a slice of bytes
+
+func main() {
+	if err := sdl.LoadLibrary(sdl.Path()); err != nil {
+		panic(err)
+	}
+	if err := ttf.LoadLibrary(ttf.Path()); err != nil {
+		panic(err)
+	}
+
+	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+	if err := ttf.Init(); err != nil {
+		panic(err)
+	}
+
+	font, err := ttf.OpenFont("resources/Roboto-Regular.ttf", 16)
+	if err != nil {
+		panic(err)
+	}
+
+	fonts := []sdl2.Font{
+		sl.FONT_ID_BODY_16: {
+			FontId: sl.FONT_ID_BODY_16,
+			Font:   font,
+		},
+	}
+
+	var (
+		window   *sdl.Window
+		renderer *sdl.Renderer
+	)
+
+	sdl.SetHint(sdl.HINT_RENDER_DRIVER, "opengl")
+
+	window, renderer, err = sdl.CreateWindowAndRenderer("SDL", 800, 600, sdl.WINDOW_RESIZABLE)
+	if err != nil {
+		panic(err)
+	}
+	_ = sdl.GL_SetAttribute(sdl.GL_MULTISAMPLESAMPLES, 4) //for antialiasing
+
+	const enableVsync = false
+	if enableVsync {
+		renderer.SetVSync(1)
+	}
+	_ = renderer.SetDrawBlendMode(sdl.BLENDMODE_BLEND) //for alpha blending
+
+	const screenWidth, screenHeight = 800, 600
+
+	totalMemorySize := clay.MinMemorySize()
+	memory := make([]byte, totalMemorySize)
+	arena := clay.CreateArenaWithCapacityAndMemory(uint64(totalMemorySize), unsafe.Pointer(unsafe.SliceData(memory)))
+	clay.Initialize(arena, clay.Dimensions{Width: screenWidth, Height: screenHeight}, clay.ErrorHandler{ErrorHandlerFunction: handleClayError})
+
+	clay.SetMeasureTextFunction(sdl2.MeasureText, unsafe.Pointer(&fonts))
+
+	var NOW = sdl.GetPerformanceCounter()
+	var LAST uint64 = 0
+	var deltaTime float64 = 0
+	var demoData = sl.ClayVideoDemo_Initialize()
+
+loop:
+	for {
+		scrollDelta := clay.Vector2{}
+		var event sdl.Event
+		for sdl.PollEvent(&event) {
+			switch event.Type {
+			case sdl.EVENT_QUIT:
+				break loop
+			case sdl.EVENT_MOUSE_WHEEL:
+				e := event.MouseWheelEvent()
+				scrollDelta.X = float32(e.X)
+				scrollDelta.Y = float32(e.Y)
+			}
+		}
+
+		LAST = NOW
+		NOW = sdl.GetPerformanceCounter()
+		deltaTime = (float64)((NOW-LAST)*1000) / (float64)(sdl.GetPerformanceFrequency())
+		fmt.Println(deltaTime)
+
+		mouseState, mouseX, mouseY := sdl.GetMouseState()
+		var mousePosition = clay.Vector2{X: float32(mouseX), Y: float32(mouseY)}
+		clay.SetPointerState(mousePosition, mouseState&sdl.ButtonMask(1) != 0)
+
+		clay.UpdateScrollContainers(
+			true,
+			clay.Vector2{X: scrollDelta.X, Y: scrollDelta.Y},
+			float32(deltaTime),
+		)
+
+		windowWidth, windowHeight, _ := window.Size()
+		clay.SetLayoutDimensions(clay.Dimensions{Width: float32(windowWidth), Height: float32(windowHeight)})
+
+		renderCommands := sl.ClayVideoDemo_CreateLayout(&demoData)
+		_ = renderer.SetDrawColor(0, 0, 0, 255)
+		_ = renderer.Clear()
+
+		sdl2.ClayRender(renderer, renderCommands, fonts)
+
+		renderer.Present()
+	}
+
+}
---
+++ renderers/sdl3/sdl3.go
@@ -0,0 +1,370 @@
+package sdl2
+
+import (
+	"fmt"
+	"log/slog"
+	"math"
+	"strings"
+	"unsafe"
+
+	sdl "github.com/Zyko0/go-sdl3"
+	"github.com/Zyko0/go-sdl3/ttf"
+	"github.com/totallygamerjet/clay"
+)
+
+type Font struct {
+	FontId uint32
+	Font   *ttf.Font
+}
+
+func MeasureText(text clay.StringSlice, config *clay.TextElementConfig, userData unsafe.Pointer) clay.Dimensions {
+	fonts := *(*[]Font)(userData)
+	font := fonts[config.FontId].Font
+
+	width, height, err := font.StringSize(text.String())
+	if err != nil {
+		panic(err)
+	}
+
+	return clay.Dimensions{
+		Width:  float32(width),
+		Height: float32(height),
+	}
+}
+
+func ClayRender(renderer *sdl.Renderer, renderCommands clay.RenderCommandArray, fonts []Font) {
+	for i := int32(0); i < renderCommands.Length; i++ {
+		renderCommand := clay.RenderCommandArray_Get(&renderCommands, i)
+		boundingBox := renderCommand.BoundingBox
+		switch renderCommand.CommandType {
+		case clay.RENDER_COMMAND_TYPE_RECTANGLE:
+			config := &renderCommand.RenderData.Rectangle
+			color := config.BackgroundColor
+			if err := renderer.SetDrawColor(uint8(color.R), uint8(color.G), uint8(color.B), uint8(color.A)); err != nil {
+				panic(err)
+			}
+			rect := sdl.FRect{
+				X: boundingBox.X,
+				Y: boundingBox.Y,
+				W: boundingBox.Width,
+				H: boundingBox.Height,
+			}
+			if config.CornerRadius.TopLeft > 0 {
+				// TODO: SDL_RenderFillRoundedRect(renderer, rect, config->cornerRadius.topLeft, color);
+				if err := RenderFillRoundedRect(renderer, rect, config.CornerRadius.TopLeft, color); err != nil {
+					panic(err)
+				}
+			} else {
+				if err := renderer.RenderFillRect(&rect); err != nil {
+					panic(err)
+				}
+			}
+		case clay.RENDER_COMMAND_TYPE_TEXT:
+			config := &renderCommand.RenderData.Text
+			cloned := strings.Clone(config.StringContents.String())
+			font := fonts[config.FontId].Font
+			surface, err := font.RenderTextBlended(cloned, sdl.Color{
+				R: uint8(config.TextColor.R),
+				G: uint8(config.TextColor.G),
+				B: uint8(config.TextColor.B),
+				A: uint8(config.TextColor.A),
+			})
+			if err != nil {
+				panic(err)
+			}
+			texture, err := renderer.CreateTextureFromSurface(surface)
+			if err != nil {
+				panic(err)
+			}
+			destination := sdl.FRect{
+				X: boundingBox.X,
+				Y: boundingBox.Y,
+				W: boundingBox.Width,
+				H: boundingBox.Height,
+			}
+			if err := renderer.RenderTexture(texture, nil, &destination); err != nil {
+				panic(err)
+			}
+			texture.Destroy()
+			surface.Destroy()
+		case clay.RENDER_COMMAND_TYPE_SCISSOR_START:
+			currentClippingRectangle := sdl.Rect{
+				X: int32(boundingBox.X),
+				Y: int32(boundingBox.Y),
+				W: int32(boundingBox.Width),
+				H: int32(boundingBox.Height),
+			}
+			if err := renderer.SetClipRect(&currentClippingRectangle); err != nil {
+				panic(err)
+			}
+		case clay.RENDER_COMMAND_TYPE_SCISSOR_END:
+			if err := renderer.SetClipRect(nil); err != nil {
+				panic(err)
+			}
+		case clay.RENDER_COMMAND_TYPE_IMAGE:
+			config := &renderCommand.RenderData.Image
+			texture, err := renderer.CreateTextureFromSurface((*sdl.Surface)(config.ImageData))
+			if err != nil {
+				panic(err)
+			}
+			destination := sdl.FRect{
+				X: boundingBox.X,
+				Y: boundingBox.Y,
+				W: boundingBox.Width,
+				H: boundingBox.Height,
+			}
+			if err := renderer.RenderTexture(texture, nil, &destination); err != nil {
+				panic(err)
+			}
+			texture.Destroy()
+		case clay.RENDER_COMMAND_TYPE_BORDER:
+			panic("not implemented")
+			//Clay_BorderRenderData *config = &renderCommand->renderData.border;
+			//SDL_SetRenderDrawColor(renderer, CLAY_COLOR_TO_SDL_COLOR_ARGS(config->color));
+			//
+			//if(boundingBox.width > 0 & boundingBox.height > 0){
+			//   const float maxRadius = SDL_min(boundingBox.width, boundingBox.height) / 2.0f;
+			//
+			//   if (config->width.left > 0) {
+			//	   const float clampedRadiusTop = SDL_min((float)config->cornerRadius.topLeft, maxRadius);
+			//	   const float clampedRadiusBottom = SDL_min((float)config->cornerRadius.bottomLeft, maxRadius);
+			//	   SDL_FRect rect = {
+			//		   boundingBox.x,
+			//		   boundingBox.y + clampedRadiusTop,
+			//		   (float)config->width.left,
+			//		   (float)boundingBox.height - clampedRadiusTop - clampedRadiusBottom
+			//	   };
+			//	   SDL_RenderFillRectF(renderer, &rect);
+			//   }
+			//
+			//   if (config->width.right > 0) {
+			//	   const float clampedRadiusTop = SDL_min((float)config->cornerRadius.topRight, maxRadius);
+			//	   const float clampedRadiusBottom = SDL_min((float)config->cornerRadius.bottomRight, maxRadius);
+			//	   SDL_FRect rect = {
+			//		   boundingBox.x + boundingBox.width - config->width.right,
+			//		   boundingBox.y + clampedRadiusTop,
+			//		   (float)config->width.right,
+			//		   (float)boundingBox.height - clampedRadiusTop - clampedRadiusBottom
+			//	   };
+			//	   SDL_RenderFillRectF(renderer, &rect);
+			//   }
+			//
+			//   if (config->width.top > 0) {
+			//	   const float clampedRadiusLeft = SDL_min((float)config->cornerRadius.topLeft, maxRadius);
+			//	   const float clampedRadiusRight = SDL_min((float)config->cornerRadius.topRight, maxRadius);
+			//	   SDL_FRect rect = {
+			//		   boundingBox.x + clampedRadiusLeft,
+			//		   boundingBox.y,
+			//		   boundingBox.width - clampedRadiusLeft - clampedRadiusRight,
+			//		   (float)config->width.top };
+			//	   SDL_RenderFillRectF(renderer, &rect);
+			//   }
+			//
+			//   if (config->width.bottom > 0) {
+			//	   const float clampedRadiusLeft = SDL_min((float)config->cornerRadius.bottomLeft, maxRadius);
+			//	   const float clampedRadiusRight = SDL_min((float)config->cornerRadius.bottomRight, maxRadius);
+			//	   SDL_FRect rect = {
+			//		   boundingBox.x + clampedRadiusLeft,
+			//		   boundingBox.y + boundingBox.height - config->width.bottom,
+			//		   boundingBox.width - clampedRadiusLeft - clampedRadiusRight,
+			//		   (float)config->width.bottom
+			//	   };
+			//	   SDL_RenderFillRectF(renderer, &rect);
+			//   }
+			//
+			//   //corner index: 0->3 topLeft -> CW -> bottonLeft
+			//   if (config->width.top > 0 & config->cornerRadius.topLeft > 0) {
+			//	   SDL_RenderCornerBorder(renderer, &boundingBox, config, 0, config->color);
+			//   }
+			//
+			//   if (config->width.top > 0 & config->cornerRadius.topRight> 0) {
+			//	   SDL_RenderCornerBorder(renderer, &boundingBox, config, 1, config->color);
+			//   }
+			//
+			//   if (config->width.bottom > 0 & config->cornerRadius.bottomLeft > 0) {
+			//	   SDL_RenderCornerBorder(renderer, &boundingBox, config, 2, config->color);
+			//   }
+			//
+			//   if (config->width.bottom > 0 & config->cornerRadius.bottomLeft > 0) {
+			//	   SDL_RenderCornerBorder(renderer, &boundingBox, config, 3, config->color);
+			//   }
+			//}
+		case clay.RENDER_COMMAND_TYPE_NONE:
+		case clay.RENDER_COMMAND_TYPE_CUSTOM:
+		default:
+			slog.Warn("Unknown command type", "type", renderCommand.CommandType)
+		}
+	}
+}
+
+const NUM_CIRCLE_SEGMENTS = 16
+
+func RenderFillRoundedRect(renderer *sdl.Renderer, rect sdl.FRect, cornerRadius float32, _color clay.Color) error {
+	color := sdl.FColor{
+		R: _color.R,
+		G: _color.G,
+		B: _color.B,
+		A: _color.A,
+	}
+
+	indexCount, vertexCount := 0, int32(0)
+
+	maxRadius := min(rect.W, rect.H) / 2.0
+	clampedRadius := min(cornerRadius, maxRadius)
+
+	numCircleSegments := int(max(NUM_CIRCLE_SEGMENTS, clampedRadius*0.5)) // check if needs to be clamped
+
+	var vertices [512]sdl.Vertex
+	var indices [512]int32
+
+	//define center rectangle
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X + clampedRadius, Y: rect.Y + clampedRadius}, Color: color, TexCoord: sdl.FPoint{}} //0 center TL
+	vertexCount++
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X + rect.W - clampedRadius, Y: rect.Y + clampedRadius}, Color: color, TexCoord: sdl.FPoint{X: 1}} //1 center TR
+	vertexCount++
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X + rect.W - clampedRadius, Y: rect.Y + rect.H - clampedRadius}, Color: color, TexCoord: sdl.FPoint{X: 1, Y: 1}} //2 center BR
+	vertexCount++
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X + clampedRadius, Y: rect.Y + rect.H - clampedRadius}, Color: color, TexCoord: sdl.FPoint{Y: 1}} //3 center BL
+	vertexCount++
+
+	indices[indexCount] = 0
+	indexCount++
+	indices[indexCount] = 1
+	indexCount++
+	indices[indexCount] = 3
+	indexCount++
+	indices[indexCount] = 1
+	indexCount++
+	indices[indexCount] = 2
+	indexCount++
+	indices[indexCount] = 3
+	indexCount++
+
+	//define rounded corners as triangle fans
+	step := (math.Pi / 2) / float32(numCircleSegments)
+	for i := 0; i < numCircleSegments; i++ {
+		angle1 := float32(i) * step
+		angle2 := (float32(i) + 1) * step
+
+		for j := int32(0); j < 4; j++ {
+			var cx, cy, signX, signY float32
+
+			switch j {
+			case 0:
+				cx = rect.X + clampedRadius
+				cy = rect.Y + clampedRadius
+				signX = -1
+				signY = -1
+				break // Top-left
+			case 1:
+				cx = rect.X + rect.W - clampedRadius
+				cy = rect.Y + clampedRadius
+				signX = 1
+				signY = -1
+				break // Top-right
+			case 2:
+				cx = rect.X + rect.W - clampedRadius
+				cy = rect.Y + rect.H - clampedRadius
+				signX = 1
+				signY = 1
+				break // Bottom-right
+			case 3:
+				cx = rect.X + clampedRadius
+				cy = rect.Y + rect.H - clampedRadius
+				signX = -1
+				signY = 1
+				break // Bottom-left
+			default:
+				return fmt.Errorf("out of bounds index: %d", j)
+			}
+
+			vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: cx + float32(math.Cos(float64(angle1)))*clampedRadius*signX, Y: cy + float32(math.Sin(float64(angle1)))*clampedRadius*signY}, Color: color, TexCoord: sdl.FPoint{}}
+			vertexCount++
+			vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: cx + float32(math.Cos(float64(angle2)))*clampedRadius*signX, Y: cy + float32(math.Sin(float64(angle2)))*clampedRadius*signY}, Color: color, TexCoord: sdl.FPoint{}}
+			vertexCount++
+
+			indices[indexCount] = j // Connect to corresponding central rectangle vertex
+			indexCount++
+			indices[indexCount] = vertexCount - 2
+			indexCount++
+			indices[indexCount] = vertexCount - 1
+			indexCount++
+		}
+	}
+	//Define edge rectangles
+	// Top edge
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X + clampedRadius, Y: rect.Y}, Color: color, TexCoord: sdl.FPoint{}} //TL
+	vertexCount++
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X + rect.W - clampedRadius, Y: rect.Y}, Color: color, TexCoord: sdl.FPoint{X: 1}} //TR
+	vertexCount++
+
+	indices[indexCount] = 0
+	indexCount++
+	indices[indexCount] = vertexCount - 2 //TL
+	indexCount++
+	indices[indexCount] = vertexCount - 1 //TR
+	indexCount++
+	indices[indexCount] = 1
+	indexCount++
+	indices[indexCount] = 0
+	indexCount++
+	indices[indexCount] = vertexCount - 1 //TR
+	indexCount++
+	// Right edge
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X + rect.W, Y: rect.Y + clampedRadius}, Color: color, TexCoord: sdl.FPoint{X: 1}} //RT
+	vertexCount++
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X + rect.W, Y: rect.Y + rect.H - clampedRadius}, Color: color, TexCoord: sdl.FPoint{X: 1, Y: 1}} //RB
+	vertexCount++
+
+	indices[indexCount] = 1
+	indexCount++
+	indices[indexCount] = vertexCount - 2 //RT
+	indexCount++
+	indices[indexCount] = vertexCount - 1 //RB
+	indexCount++
+	indices[indexCount] = 2
+	indexCount++
+	indices[indexCount] = 1
+	indexCount++
+	indices[indexCount] = vertexCount - 1 //RB
+	indexCount++
+	// Bottom edge
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X + rect.W - clampedRadius, Y: rect.Y + rect.H}, Color: color, TexCoord: sdl.FPoint{X: 1, Y: 1}} //BR
+	vertexCount++
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X + clampedRadius, Y: rect.Y + rect.H}, Color: color, TexCoord: sdl.FPoint{Y: 1}} //BL
+	vertexCount++
+
+	indices[indexCount] = 2
+	indexCount++
+	indices[indexCount] = vertexCount - 2 //BR
+	indexCount++
+	indices[indexCount] = vertexCount - 1 //BL
+	indexCount++
+	indices[indexCount] = 3
+	indexCount++
+	indices[indexCount] = 2
+	indexCount++
+	indices[indexCount] = vertexCount - 1 //BL
+	indexCount++
+	// Left edge
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X, Y: rect.Y + rect.H - clampedRadius}, Color: color, TexCoord: sdl.FPoint{Y: 1}} //LB
+	vertexCount++
+	vertices[vertexCount] = sdl.Vertex{Position: sdl.FPoint{X: rect.X, Y: rect.Y + clampedRadius}, Color: color, TexCoord: sdl.FPoint{}} //LT
+	vertexCount++
+
+	indices[indexCount] = 3
+	indexCount++
+	indices[indexCount] = vertexCount - 2 //LB
+	indexCount++
+	indices[indexCount] = vertexCount - 1 //LT
+	indexCount++
+	indices[indexCount] = 0
+	indexCount++
+	indices[indexCount] = 3
+	indexCount++
+	indices[indexCount] = vertexCount - 1 //LT
+	indexCount++
+
+	// Render everything
+	return renderer.RenderGeometry(nil, vertices[:vertexCount], indices[:indexCount])
+}
